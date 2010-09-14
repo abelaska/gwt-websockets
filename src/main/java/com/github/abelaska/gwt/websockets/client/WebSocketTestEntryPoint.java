5d93ca2d0a9ea19cff41103d5bf8982693687f46
Alois Bělaška <alois.belaska@gmail.com>
2010-09-14 11:17:58 +0200

    Restrukturalizace projektu

--- src/main/java/com/github/abelaska/gwt/websockets/client/WebSocketTestEntryPoint.java
+++ src/main/java/com/github/abelaska/gwt/websockets/client/WebSocketTestEntryPoint.java
@@ -1,38 +1,34 @@
 package com.github.abelaska.gwt.websockets.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 
-public class WebSocketsTestEntryPoint implements EntryPoint {
+public class WebSocketTestEntryPoint implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-//		WebSockets ws = new WebSockets();
-//		ws.open("ws://127.0.0.1:10081");
-//		ws.close();
 		WebSocket ws = WebSocket.create("ws://127.0.0.1:10081");
 		ws.setHandler(new WebSocketHandler() {
 			
 			@Override
 			public void onOpen(WebSocket webSocket) {
+				Window.alert("open");
 				webSocket.send("ahoj2");
 			}
 			
 			@Override
 			public void onMessage(WebSocket webSocket, MessageEvent event) {
-				// TODO Auto-generated method stub
-				
+				Window.alert("message: " + event.getData());
 			}
 			
 			@Override
 			public void onError(WebSocket webSocket) {
-				// TODO Auto-generated method stub
-				
+				Window.alert("error");
 			}
 			
 			@Override
 			public void onClose(WebSocket webSocket) {
-				// TODO Auto-generated method stub
-				
+				Window.alert("close");
 			}
 		});
 	}
